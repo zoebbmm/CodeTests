b10c3c59cb9f1e2b5fc4564890dd13999fe81331
Wei Zhou <weizhou@Weis-MacBook-Pro.local>
2017-04-18 13:53:02 -0700

    finished function

--- src/GenericConceptTests/TestGit/Main.java
+++ src/GenericConceptTests/TestGit/Main.java
@@ -4,4 +4,7 @@ package GenericConceptTests.TestGit;
  * Created by weizhou on 4/18/17.
  */
 public class Main {
+    public static void main(String[] agrs) {
+        System.out.println("hello world");
+    }
 }
